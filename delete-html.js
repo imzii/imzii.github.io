0a3d5403f33a48019707e93486a43bf6aa021617
Jihoon Lim <110262168+imzii@users.noreply.github.com>
2023-11-20 17:06:14 +0900

    Update delete-html.js

--- delete-html.js
+++ delete-html.js
@@ -1,1 +1,17 @@
+const fs = require('fs');
 
+function removeElementsByKeyValue(arr, key, value) {
+  return arr.filter(item => item[key] !== value);
+}
+
+const portfolioData = require('./portfolio/portfolio.json');
+const issueNumberToDelete = issues.issue.number;
+const indexToDelete = portfolioData.findIndex(item => item.number === issueNumberToDelete);
+
+if (indexToDelete !== -1) {
+  portfolioData.splice(indexToDelete, 1);
+  fs.writeFileSync('./portfolio/portfolio.json', JSON.stringify(portfolioData, null, 2));
+  console.log(`Issue ${issueNumberToDelete} removed from portfolio.json`);
+} else {
+  console.log(`Issue ${issueNumberToDelete} not found in portfolio.json`);
+}
